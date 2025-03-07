e4ac5e900b05d909d45a4d8861e83aae1adf488e
Aishaleem1 <aishaaroses@gmail.com>
2025-03-07 16:41:42 +0300

    my first commit

---
+++ tfcode/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
